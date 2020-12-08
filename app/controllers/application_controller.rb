7fdc6ed666f8d7359cce1da470e312c2ecb4cb4b
zaynegoering <zaynegoering@gmail.com>
2020-12-07 17:24:43 -0800

    create seperate user_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,47 +12,5 @@ class ApplicationController < Sinatra::Base
   get "/" do
     erb :index
   end
-
-  post "/user" do
-    user = User.find_by(username: params[:username])
-    
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      # binding.pry
-      redirect "/user/#{user.id}"
-    else
-      redirect "/user/new"
-    end
-  end
-
-  get "/user/new" do
-      erb :user_new
-  end
-
-  post "/user/logout" do
-    session.clear
-    redirect "/"
-  end
-
-  get "/user/:id" do
-    # binding.pry
-    if session[:user_id] == params[:id].to_i 
-      @user = User.find(params[:id])
-      erb :user
-    else
-      redirect "/user/#{session[:user_id]}"
-    end
-  end
-
-  post "/user/new" do 
-    user = User.create(params)
-    redirect "/user/#{user.id}"
-  end
-
-  delete "/user/:id" do
-    user = User.find(params[:id])
-    user.delete
-    redirect "/"
-  end
-
+  
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,16 +3,50 @@ require './config/environment'
 class UserController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, "secret" # CHANGE THIS TO SOMETHING MORE SECURE
     set :public_folder, 'public'
-    set :views, 'app/views'
-  end
-
-  get "/user/new" do
-    erb :user_new
+    set :views, 'app/views/user_views'
   end
 
   post "/user" do
+    user = User.find_by(username: params[:username])
+    
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/user/#{user.id}"
+    else
+      redirect "/user/new"
+    end
+  end
 
+  get "/user/new" do
+      erb :user_new
+  end
+
+  post "/user/logout" do
+    session.clear
+    redirect "/"
+  end
+
+  get "/user/:id" do
+    if session[:user_id] == params[:id].to_i 
+      @user = User.find(params[:id])
+      erb :user
+    else
+      redirect "/user/#{session[:user_id]}"
+    end
+  end
+
+  post "/user/new" do 
+    user = User.create(params)
+    redirect "/user/#{user.id}"
+  end
+
+  delete "/user/:id" do
+    user = User.find(params[:id])
+    user.delete
+    redirect "/"
   end
 
 end
