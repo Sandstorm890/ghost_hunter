4e2b4dbaf8919ad5171941c604ed599ce7664759
zaynegoering <zaynegoering@gmail.com>
2020-12-11 14:02:25 -0800

    adds .user_job method

---
+++ app/helpers/helpers.rb
@@ -0,0 +1,16 @@
+require 'sinatra/base'
+
+class Helpers
+
+  def self.current_user(session)
+    @user = User.find_by_id(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    !!session[:user_id]
+  end
+
+  def self.user_job(job_id)
+    UserJob.find_by(job_id: job_id)
+  end
+end
