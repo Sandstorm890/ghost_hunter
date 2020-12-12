a3c822e6ee2d4a3b74ba1dc1db7c37e817e3226c
zaynegoering <zaynegoering@gmail.com>
2020-12-12 15:48:41 -0800

    define SESSION_SECRET env variable

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,9 @@
 ENV['SINATRA_ENV'] ||= "development"
-
+SESSION_SECRET = "21c2a6478ef1ca54440df77f24d2011acb9b28af9fe23d64d28bf5b264f911c3a1f1e7a39328d0629f238f4ef09416800f62de767a5422092aa20adfa2ca35f9" 
 require 'bundler/setup'
 require 'pry'
 require 'sanitize'
+require 'securerandom'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
