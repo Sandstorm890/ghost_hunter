5e444ad8ce7108013662ebe44fe1a301cfa99d54
zaynegoering <zaynegoering@gmail.com>
2020-12-13 13:53:35 -0800

    remove tools association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,4 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :user_jobs
     has_many :jobs, through: :user_jobs
-    # has many :tools, through: :user_jobs # this is causing a no method error
 end
