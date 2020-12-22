c98fff1e9077db05097907ba5802783968ca8ee6
zaynegoering <zaynegoering@gmail.com>
2020-12-22 15:22:27 -0800

    makes it DRY and implements helpers

--- app/controllers/job_controller.rb
+++ app/controllers/job_controller.rb
@@ -3,8 +3,8 @@ require './config/environment'
 class JobController < ApplicationController
 
     get "/jobs/:id/edit" do
-        if UserJob.find_by(job_id: params[:id]).user_id == session[:user_id]
-            @job = Job.find(params[:id])
+        if logged_in? && user_job(params[:id]).user_id == session[:user_id]
+            current_job
             erb :"/job_views/job_edit"
         else
             erb :"/failure"
@@ -23,7 +23,7 @@ class JobController < ApplicationController
         if params[:location] == ""
             erb :job_create_failure
         else
-            clean_params = sanitize_params
+            clean_params = sanitize_params # is this more efficient than calling 'sanitize_params' a bunch in the arguments?
             job = Job.create(location: clean_params[:location], difficulty: clean_params[:difficulty], ghost_type: params[:ghost_type], date: params[:date], description: clean_params[:description])
             UserJob.create(job_id: job.id, user_id: session[:user_id])
             redirect "/user/#{session[:user_id]}"
@@ -37,22 +37,20 @@ class JobController < ApplicationController
     end
 
     delete "/jobs/:id" do
-        @user_job = UserJob.find_by(job_id: params[:id])
-        @job = @user_job.job
         if valid_owner?
-            @job.destroy
+            user_job.destroy
         else
-            redirect :"../failure"
+            erb :failure
         end
         redirect "/user/#{session[:user_id]}"
     end
 
     get "/jobs/:id" do
         if Job.find_by(id: params[:id])
-            @job = Job.find(params[:id])
+            current_job
             erb :"/job_views/job"
         else
-            erb :"../failure"
+            erb :failure
         end
     end
 
@@ -66,7 +64,7 @@ class JobController < ApplicationController
     end
 
     def current_job
-        Job.find(params[:id])
+        @job = Job.find(params[:id])
     end
 
     def current_user_job
