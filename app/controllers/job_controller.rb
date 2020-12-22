aa932fa19051f0eac32854c342f272a36c31cd82
zaynegoering <zaynegoering@gmail.com>
2020-12-22 14:23:00 -0800

    move config to app controller & DRY sanitize

--- app/controllers/job_controller.rb
+++ app/controllers/job_controller.rb
@@ -1,49 +1,45 @@
 require './config/environment'
 
-class JobController < Sinatra::Base
-
-    configure do
-        enable :sessions
-        set :session_secret, SESSION_SECRET
-        set :public_folder, 'public'
-        set :views, 'app/views/job_views'
-    end
+class JobController < ApplicationController
 
     get "/jobs/:id/edit" do
         if UserJob.find_by(job_id: params[:id]).user_id == session[:user_id]
             @job = Job.find(params[:id])
-            erb :job_edit
+            erb :"/job_views/job_edit"
         else
             erb :"../failure"
         end
     end
 
     get "/jobs/new" do
-        erb :job_new
+        # check if logged in
+        erb :"/job_views/job_new"
     end
     
     post "/jobs" do
-        location = Sanitize.clean(params[:location])
-        difficulty = Sanitize.clean(params[:difficulty])
-        description = Sanitize.clean(params[:description])
-        job = Job.create(location: location, difficulty: difficulty, ghost_type: params[:ghost_type], date: params[:date], description: description)
-        UserJob.create(job_id: job.id, user_id: session[:user_id])
-        redirect "/user/#{session[:user_id]}"
+        if params[:location] == ""
+            erb :job_create_failure
+        else
+            clean_params = sanitize_params
+            job = Job.create(location: clean_params[:location], difficulty: clean_params[:difficulty], ghost_type: params[:ghost_type], date: params[:date], description: clean_params[:description])
+            UserJob.create(job_id: job.id, user_id: session[:user_id])
+            redirect "/user/#{session[:user_id]}"
+        end
     end
 
     patch "/jobs/:id" do 
-        location = Sanitize.clean(params[:location])
-        difficulty = Sanitize.clean(params[:difficulty])
-        description = Sanitize.clean(params[:description])
-        Job.find(params[:id]).update(location: location, difficulty: difficulty, date: params[:date], ghost_type: params[:ghost_type], description: description)
+        clean_params = sanitize_params
+        current_job.update(location: clean_params[:location], difficulty: clean_params[:difficulty], date: params[:date], ghost_type: params[:ghost_type], description: clean_params[:description])
         redirect "/jobs/#{params[:id]}"
     end
 
     delete "/jobs/:id" do
-        @job = UserJob.find_by(job_id: params[:id])
-        if Job.find(@job.job_id) && @job.user_id == session[:user_id]
-            Job.find(@job.job_id).delete
-            @job.delete
+        @user_job = UserJob.find_by(job_id: params[:id])
+        @job = @user_job.job
+        if valid_owner?
+            @job.destroy
+        else
+            redirect :"../failure"
         end
         redirect "/user/#{session[:user_id]}"
     end
@@ -51,9 +47,27 @@ class JobController < Sinatra::Base
     get "/jobs/:id" do
         if Job.find_by(id: params[:id])
             @job = Job.find(params[:id])
-            erb :job
+            erb :"/job_views/job"
         else
             erb :"../failure"
         end
     end
+
+    private
+    def valid_owner?
+        if @job && current_user_job.user_id == session[:user_id]
+            true
+        else
+            false
+        end
+    end
+
+    def current_job
+        Job.find(params[:id])
+    end
+
+    def current_user_job
+        UserJob.find_by(job_id: params[:id])
+    end
+
 end
